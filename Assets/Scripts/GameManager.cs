120d66bac3461973e892bc6bdbc7f6f0d5185587
juan daniel lopez florez <lopezfloresjuandaniel123@gmail.com>
2018-10-22 22:49:07 -0500

    primer cambio

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GamesState
+{
+    nemu,
+    inTheGame,
+    gameOver
+}
+
+public class GameManager : MonoBehaviour {
+
+    public GamesState currentGameState = GamesState.nemu;
+
+    void Start()
+    {
+        StartGame();
+    }
+
+	// se llama para inicar el juego o la partida
+	public void StartGame () {
+        ChangeGameState(GamesState.inTheGame);
+	}
+	
+	// se llama cuando el jugador muere
+    public void GameOver()
+    {
+        ChangeGameState(GamesState.gameOver);
+    }
+
+    //se llama cuando el jugador finaliza el juego y regresa al menu principal
+    public void BackToMainMenu()
+    {
+        ChangeGameState(GamesState.nemu);
+    }
+
+    void ChangeGameState(GamesState newGamesState)
+    {
+        switch (newGamesState)
+        {
+            case GamesState.nemu:
+                //la esene debera mostrar el menu principal
+                break;
+            case GamesState.inTheGame:
+                //debe configurarse para mostrar el juego
+                break;
+            case GamesState.gameOver:
+                //la esena debe mostar la pantalla de fin de partida   
+                break;
+            default:
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float jumpForce = 6.0f;
+    public float runningSpeed = 2.5f;
+    private Rigidbody2D rigiBody;
+    public LayerMask groundLayerMask;
+    public Animator animator; 
+
+    void Awake()
+    {
+        rigiBody = GetComponent<Rigidbody2D>();
+        
+    }
+
+	// Use this for initialization
+	void Start () {
+        animator.SetBool("isAlive",true);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Jump();
+        }
+
+        animator.SetBool("isGrounded",IsOnTheFloor());
+	}
+
+    void FixedUpdate()
+    {
+        if (rigiBody.velocity.x < runningSpeed)
+            rigiBody.velocity = new Vector2(runningSpeed, rigiBody.velocity.y);
+    }
+
+    void Jump()
+    {
+        if (IsOnTheFloor())
+        {
+            rigiBody.AddForce (Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+
+    }
+
+    bool IsOnTheFloor()
+    {
+        if (Physics2D.Raycast (this.transform.position, Vector2.down, 1.0f, groundLayerMask.value))
+        {
+            return true;
+        }
+        else
+        {
+            return false;    
+        }
+            
+    }
+}
